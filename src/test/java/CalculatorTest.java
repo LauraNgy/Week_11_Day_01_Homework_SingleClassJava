6f648ff5748c46b30df7f9a3aefaece34601e66c
Laura Nagy <ngylaura@gmail.com>
2018-10-08 16:24:48 +0100

    adds tests for Calculator class

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,2 +1,34 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class CalculatorTest {
+
+    Calculator calculator;
+
+    @Before
+    public void before() {
+        calculator = new Calculator();
+    }
+
+    @Test
+    public void canAdd() {
+        assertEquals(5, calculator.add(2, 3));
+    }
+
+    @Test
+    public void canSubstract() {
+        assertEquals(3, calculator.substract(5, 2));
+    }
+
+    @Test
+    public void canMultiply() {
+        assertEquals(6, calculator.multiply(2, 3));
+    }
+
+    @Test
+    public void canDivide() {
+        assertEquals(4.00, calculator.divide(8, 2), 0.9);
+    }
 }
